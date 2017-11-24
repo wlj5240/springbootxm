18f7f90972c2516c4b198a1e1aaf87f2912c2522
fuwei.deng <442617114@qq.com>
2017-11-24 18:31:09 +0800

    修改redis分布式锁，防止设置过期时间失败的时候造成死锁的情况

--- itopener-parent/spring-boot-starters-parent/lock-redis-spring-boot-starter-parent/lock-redis-spring-boot-autoconfigure/src/main/java/com/itopener/lock/redis/spring/boot/autoconfigure/lock/RedisDistributedLock.java
+++ itopener-parent/spring-boot-starters-parent/lock-redis-spring-boot-starter-parent/lock-redis-spring-boot-autoconfigure/src/main/java/com/itopener/lock/redis/spring/boot/autoconfigure/lock/RedisDistributedLock.java
@@ -1,10 +1,14 @@
 package com.itopener.lock.redis.spring.boot.autoconfigure.lock;
 
-import java.util.concurrent.TimeUnit;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.StringUtils;
+
+import redis.clients.jedis.JedisCommands;
 
 /**
  * @author fuwei.deng
@@ -24,33 +28,42 @@ public class RedisDistributedLock extends AbstractDistributedLock {
 
 	@Override
 	public synchronized boolean lock(String key, long expire, int retryTimes, long sleepMillis) {
-		Boolean result = redisTemplate.opsForValue().setIfAbsent(key, 1L);
-		while((result == null || !result.booleanValue()) && retryTimes-- > 0){
+		boolean result = setRedis(key, expire);
+		// 如果获取锁失败，按照传入的重试次数进行重试
+		while((!result) && retryTimes-- > 0){
 			try {
 				logger.info("lock failed, retrying..." + retryTimes);
 				Thread.sleep(sleepMillis);
 			} catch (InterruptedException e) {
 				return false;
 			}
-			result = redisTemplate.opsForValue().setIfAbsent(key, 1L);
+			result = setRedis(key, expire);
 		}
-		if(result != null && result.booleanValue()){
-			result = redisTemplate.boundValueOps(key).expire(expire, TimeUnit.MILLISECONDS);
-			if(result != null && result.booleanValue()){
-				return true;
+		return result;
+	}
+	
+	private boolean setRedis(String key, long expire) {
+		String result = redisTemplate.execute(new RedisCallback<String>() {
+			@Override
+			public String doInRedis(RedisConnection connection) throws DataAccessException {
+				JedisCommands commands = (JedisCommands) connection.getNativeConnection();
+				return commands.set(key, String.valueOf(System.currentTimeMillis()), "NX", "PX", expire);
 			}
-		}
-		return false;
+		});
+		return !StringUtils.isEmpty(result);
 	}
 
 	@Override
 	public boolean releaseLock(String key) {
-		try {
-			redisTemplate.delete(key);
-		} catch (Exception e) {
-			return false;
-		}
-		return true;
+		// TODO 释放锁的时候，有可能因为持锁之后方法执行时间大于锁的有效期，此时有可能已经被另外一个线程持有锁，所以不能直接删除
+		Long result = redisTemplate.execute(new RedisCallback<Long>() {
+			@Override
+			public Long doInRedis(RedisConnection connection) throws DataAccessException {
+				JedisCommands commands = (JedisCommands) connection.getNativeConnection();
+				return commands.del(key);
+			}
+		});
+		return result != null && result > 0;
 	}
 
 }
