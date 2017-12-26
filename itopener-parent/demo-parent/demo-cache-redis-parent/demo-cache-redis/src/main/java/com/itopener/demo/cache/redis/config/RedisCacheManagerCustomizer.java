54f191779d6f57451bfd803364aa2454946d29dd
fuwei.deng <442617114@qq.com>
2017-12-26 17:57:51 +0800

    redis缓存demo（基于方法注解）增加设置指定缓存的过期时间方式

--- itopener-parent/demo-parent/demo-cache-redis-parent/demo-cache-redis/src/main/java/com/itopener/demo/cache/redis/config/RedisCacheManagerCustomizer.java
+++ itopener-parent/demo-parent/demo-cache-redis-parent/demo-cache-redis/src/main/java/com/itopener/demo/cache/redis/config/RedisCacheManagerCustomizer.java
@@ -1,5 +1,8 @@
 package com.itopener.demo.cache.redis.config;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
 import org.springframework.data.redis.cache.RedisCacheManager;
 
@@ -10,6 +13,9 @@ public class RedisCacheManagerCustomizer implements CacheManagerCustomizer<Redis
 		// 默认过期时间，单位秒
 		cacheManager.setDefaultExpiration(1000);
 		cacheManager.setUsePrefix(false);
+		Map<String, Long> expires = new ConcurrentHashMap<String, Long>();
+		expires.put("userIdCache", 2000L);
+		cacheManager.setExpires(expires);
 	}
 
 }
