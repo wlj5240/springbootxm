8ca0491c11f26e1950128daea404415b85d09794
fuwei.deng <442617114@qq.com>
2018-01-10 17:07:40 +0800

    redisson starter增加基于注解的缓存（Cacheable）的配置 增加redisson cacheable的示例

--- itopener-parent/spring-boot-starters-parent/redisson-spring-boot-starter-parent/redisson-spring-boot-autoconfigure/src/main/java/com/itopener/redisson/spring/boot/autoconfigure/RedissonProperties.java
+++ itopener-parent/spring-boot-starters-parent/redisson-spring-boot-starter-parent/redisson-spring-boot-autoconfigure/src/main/java/com/itopener/redisson/spring/boot/autoconfigure/RedissonProperties.java
@@ -1,5 +1,8 @@
 package com.itopener.redisson.spring.boot.autoconfigure;
 
+import java.util.Map;
+
+import org.redisson.spring.cache.CacheConfig;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import com.itopener.redisson.spring.boot.autoconfigure.config.Config;
@@ -13,9 +16,14 @@ import com.itopener.redisson.spring.boot.autoconfigure.config.ConfigFile;
 @ConfigurationProperties(prefix = "spring.redisson")
 public class RedissonProperties {
 	
+	/** 属性的配置方式*/
 	private Config config;
 	
+	/** 文件的配置方式*/
     private ConfigFile configFile = new ConfigFile();
+
+    /** 方法注解缓存的配置*/
+	private Map<String, CacheConfig> caches;
 
 	public Config getConfig() {
 		return config;
@@ -31,6 +39,14 @@ public class RedissonProperties {
 
 	public void setConfigFile(ConfigFile configFile) {
 		this.configFile = configFile;
+	}
+
+	public Map<String, CacheConfig> getCaches() {
+		return caches;
+	}
+
+	public void setCaches(Map<String, CacheConfig> caches) {
+		this.caches = caches;
 	}
 	
 }
